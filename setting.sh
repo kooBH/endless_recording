d0c2e89ef87b88ccdc2ad7aa6564a49b7657ec2c
KBH <k01032633280@gmail.com>
2018-07-24 16:38:27 +0900

    init

---
+++ setting.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+echo 필수 라이브러리를 설치합니다.
+sudo apt-get install libasound2-dev
+echo 녹음 장치를 탐색합니다.
+./audioprobe
+
+echo "사용할 녹음 장치는 몇번 장치입니까? (0번부터 시작)"
+read device
+while true
+do
+	echo "$device 번 장치가 맞습니까?[Y/N]"
+	read ans
+	if [ "$ans" = "Y" ];
+	then
+		break
+	else
+		echo "사용활 녹음 장치는 몇번 장치입니까? (0번 부터시작)"
+		read device
+	fi
+done
+echo $device 번 장치를 사용합니다.
+
+echo 몇 채널로 녹음합니까?
+read channels
+
+while true
+do
+	echo "$channels 가 맞습니까?[Y/N]"
+	read ans
+	if [ "$ans" = "Y" ];
+	then
+		break
+	else
+		echo 몇 채널로 녹음합니까?
+	 	read channels
+	fi
+done
+
+
+echo 샘플 레이트는 몇 입니까?
+read rates
+
+while true
+do
+	echo "$rates 가 맞습니까?[Y/N]"	
+	read ans
+	if [ "$ans" = "Y" ];
+	then
+		break
+	else
+		echo 샘플 레이트는 몇입니까?
+		read rates
+	fi
+done
+
+
+#sudo fdisk -l
+df -h
+echo "사용할 저장 장치의 경로(Mounted on)를 입력해 주십시오."
+echo "예)/media/user/Device Name"
+read path
+#df -h | grep $disk | cat 
+
+while true
+do
+	if [ -d "$path" ]
+		then
+			echo "$path" 가 존재합니다.
+			if [ ! -d "$path"/wavs ]
+			then	
+				sudo mkdir "$path"/wavs
+				echo "$path"/wavs 폴더를 만들었습니다.
+			fi	
+			break;
+		else
+			echo "$path" 는 없습니다.
+			echo "경로가 존재하지 않습니다. 다시 입력해주세요."
+			read path
+	fi
+done
+echo $path 를 사용합니다.
+
+sudo chmod 777 "$path"/wavs
+
+echo startup application을 설정합니다.
+
+echo "#!/bin/sh\npath=\"$path\"\nsleep 6\nif [ -d \"\$path\" ]\nthen\nif [ ! -d \"\$path\"/wavs ]\nthen\nmkdir \"\$path\"/wavs\nfi\ngnome-terminal -x 'bash' -c '$PWD/monitor \"$path\" $device $rates $channels 60 \"$PWD\";bash'\nelse\necho 경로가 존재하지 않습니다. setting을 다시 해주세요.\nfi ">rt_run.sh
+chmod +x rt_run.sh
+if [ ! -d ~/.config/autostart ]
+then
+	mkdir ~/.config/autostart
+fi
+
+echo "[Desktop Entry]\nType=Application\nExec=$PWD/rt_run.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=rt_run\nName=rt_run\nComment[en_US]=\nComment=" > ~/.config/autostart/rt_run.sh.desktop
+
+echo "rt_run.sh 가 startup application으로 등록되었습니다.\n현재 환경과 동일한 환경으로 부팅시 실행됩니다.\n변경된 사항이 있으면 제대로 동작하지 않을 수 있습니다.\n환경이 바뀌었을 경우엔 setting.sh를 다시 실행해 주세요."
+
+./monitor "$path" $device $rates $channels 60 "$PWD" 
+
